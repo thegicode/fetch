35c3ac251753477f5ae4b177fcd6c6f83f39f828
thegi.code <thegi.code@gmail.com>
2021-10-17 17:45:23 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,17 +1,27 @@
 
-
 fetch('https://api.github.com/users/thegicode', {
     headers: {
         accept: 'application/vnd.github.v3+json'
     }
 })
-.then(response => response.json() )
-.then( data => {
-    console.log(data);
+.then(response => {
+    switch(response.status) {
+        case 200:
+            return response.json();
+            break;
+        case 404:
+            console.log('유효하지 않은 API 요청입니다.');
+			break;
+        default:
+            console.log(response.status);
+            break;
+    }
+})
+.then(data => {
+    console.log('data', data);
     displayDom(data.login);
 })
 .catch(errer => console.log(error))
-
 
 function displayDom(login) {
     document.querySelector('#root').textContent = login;
